d2e855c1f45fed01831460ab191f6fc960d47b29
maricarril <parametrojuridico@gmail.com>
2026-02-28 20:24:53 -0300

    fix: remove dist and .vs from tracking

--- dist/app/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-I5Z55VM7.mjs";import"./chunk-ICO2X5ZS.mjs";import"./chunk-KAXRN4ZG.mjs";import"./chunk-5672UTFT.mjs";import"./chunk-6OJQON2Z.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
